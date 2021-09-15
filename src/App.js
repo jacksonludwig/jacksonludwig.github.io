cdfce2da5137f9a6eaca1199fb7f6b72e1930036
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-09-15 12:53:37 -0400

    start header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className="flex justify-center">
-      <div className="text-5xl">Jackson Ludwig</div>
+    <div className="flex justify-end items-center text-gray-800 font-mono text-2xl">
+      <div className="text-5xl mr-auto">Jackson Ludwig</div>
+      <div>linkedIn</div>
+      <div className="pl-1 pr-1">/</div>
+      <div>GitHub</div>
+      <div className="pl-1 pr-1">/</div>
+      <div>resume</div>
+      <div className="pl-1 pr-1">/</div>
+      <div>email</div>
     </div>
   );
 }
